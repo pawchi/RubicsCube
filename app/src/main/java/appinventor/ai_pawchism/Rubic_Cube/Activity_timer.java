2fc3925273af721b30610ad13169a076fc09564e
pawchi <pawchi@o2.pl>
2019-10-22 21:13:46 +0200

    timer

--- app/src/main/java/appinventor/ai_pawchism/Rubic_Cube/Activity_timer.java
+++ app/src/main/java/appinventor/ai_pawchism/Rubic_Cube/Activity_timer.java
@@ -3,6 +3,7 @@ package appinventor.ai_pawchism.Rubic_Cube;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,11 +12,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.daimajia.easing.linear.Linear;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherAdView;
 import java.util.Locale;
@@ -31,7 +31,7 @@ public class Activity_timer extends AppCompatActivity {
     String testTextActionUp = "test action UP";
     String testTextOnClick = "test on click";
     Handler customHandler = new Handler();
-
+    int timerStatus = 0;
 
     Runnable updateTimerThread = new Runnable() {
         @Override
@@ -44,6 +44,33 @@ public class Activity_timer extends AppCompatActivity {
             int milliseconds = (int) (updateTime%1000);
             textTimer.setText(""+mins+":"+String.format("%2d",secs)+":"+String.format("%3d",milliseconds));
             customHandler.postDelayed(this, 0);
+        }
+    };
+
+    Runnable startCountingTime = new Runnable(){
+        @Override
+        public void run(){
+            textTimer.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.timer_text_ready)));
+        }
+    };
+    Runnable stopCountingTime = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(), "Stop timer ", Toast.LENGTH_LONG).show();
+            /*customHandler.removeCallbacks(startCountingTime);
+            int secs = (int) (updateTime/1000);
+            int mins = secs/60;
+            secs%=60;
+            int milliseconds = (int) (updateTime%1000);
+            textTimer.setText(""+mins+":"+String.format("%2d",secs)+":"+String.format("%3d",milliseconds));
+            */
+        }
+    };
+
+    Runnable resetTimer = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(), "Action Up 1 ", Toast.LENGTH_LONG).show();
         }
     };
 
@@ -70,16 +97,35 @@ public class Activity_timer extends AppCompatActivity {
         timerButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+
                 switch (motionEvent.getAction()){
                     case MotionEvent.ACTION_DOWN:
-                        startTime = SystemClock.uptimeMillis();
-                        customHandler.postDelayed(updateTimerThread, 0);
+                        if (timerStatus==0) {
+                            textTimer.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.timer_text_praparing)));
+                            customHandler.postDelayed(startCountingTime, 500);
+                        }
+                        if (timerStatus==1){
+                            //customHandler.postDelayed(stopCountingTime, 0);
+                            customHandler.removeCallbacks(updateTimerThread);
+                        }
                         return true;
 
                     case  MotionEvent.ACTION_MOVE:
+                        return false;
                         //textTimer.setTextColor(getResources().getColor(android.R.color.holo_green_light));
 
                     case MotionEvent.ACTION_UP:
+                        if (timerStatus==0) {
+                            startTime = SystemClock.uptimeMillis();
+                            customHandler.postDelayed(updateTimerThread, 0);
+                            textTimer.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.timer_text_ongoing)));
+                            Toast.makeText(getApplicationContext(), "Action Up 0 ", Toast.LENGTH_LONG).show();
+                            timerStatus=1;
+                        }
+                        if (timerStatus==1){
+                            customHandler.postDelayed(resetTimer,500);
+                        }
+                        return true;
                         //textTimer.setText(testTextActionUp);
                 }
                 return false;
